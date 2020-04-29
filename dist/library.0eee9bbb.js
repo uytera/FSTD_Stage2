e52d9fd2468e9c8a738be8c3cf72b6931629b300
uytera <uytera12@yandex.ru>
2020-04-29 11:17:30 +0500

    Added a login form

--- dist/library.0eee9bbb.js
+++ dist/library.0eee9bbb.js
@@ -10959,7 +10959,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62135" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59060" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
